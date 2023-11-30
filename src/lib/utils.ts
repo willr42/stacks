307692f36d49badc01e79ae025b5dc677e08e4d4
Will Robertson <williamdrobertson42@gmail.com>
2023-11-30 16:08:14 +1100

    chore: add drizzle & pg

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getDbConnectionString() {
+  return `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 }
