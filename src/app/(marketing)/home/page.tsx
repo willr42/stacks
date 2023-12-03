23406fe23df946432074abcbeb771968678a4dcc
Will Robertson <williamdrobertson42@gmail.com>
2023-12-03 17:38:43 +1100

    feat: move to new marketing subpage

--- src/app/(marketing)/home/page.tsx
+++ src/app/(marketing)/home/page.tsx
@@ -1,5 +1,3 @@
-//TODO: logic for if signed out, do not show, forward to /home
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Stacks",
+  description: "One place for all your RPG materials.",
+}
+
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <p>Inside non-root layout</p>Something {children}
+      </body>
+    </html>
+  )
+}
